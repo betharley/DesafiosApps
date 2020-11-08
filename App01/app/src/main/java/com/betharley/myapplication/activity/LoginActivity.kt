43445fc524e8c82d84457e781b3f7070c5d6b336
betharley <betharley@gmail.com>
2020-11-08 18:51:39 -0300

    Validação de usuario na tela de Login e Registro

--- App01/app/src/main/java/com/betharley/myapplication/activity/LoginActivity.kt
+++ App01/app/src/main/java/com/betharley/myapplication/activity/LoginActivity.kt
@@ -5,31 +5,49 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.betharley.myapplication.R
+import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity : AppCompatActivity() {
     lateinit var login_register: Button
     lateinit var login_login: Button
 
+    lateinit var login_email_edit: TextInputEditText
+    lateinit var login_password_edit: TextInputEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-
         login_register = findViewById<Button>(R.id.login_register) as Button
         login_login = findViewById<Button>(R.id.login_login) as Button
-
+        login_email_edit = findViewById(R.id.login_email_edit) as TextInputEditText
+        login_password_edit = findViewById(R.id.login_password_edit) as TextInputEditText
 
         login_register.setOnClickListener {
-            var intent: Intent = Intent(applicationContext, HomeActivity::class.java)
+            var intent: Intent = Intent(applicationContext, RegisterActivity::class.java)
             startActivity( intent )
         }
 
         login_login.setOnClickListener {
-            var intent: Intent = Intent(applicationContext, RegisterActivity::class.java)
-            startActivity( intent )
+            logarUsuario()
         }
     }
 
 
+    fun logarUsuario(){
+        var email = login_email_edit.text.toString()
+        var password = login_password_edit.text.toString()
+
+        if( email.isEmpty() || email.equals("")){
+            login_email_edit.error = "Preencha o email"
+        }else
+        if( password.isEmpty() || password.equals("")){
+            login_password_edit.error = "Preencha a senha"
+        }else{
+            var intent: Intent = Intent(applicationContext, HomeActivity::class.java)
+            startActivity( intent )
+            finish()
+        }
+    }
 
 }
--- App01/app/src/main/java/com/betharley/myapplication/activity/RegisterActivity.kt
+++ App01/app/src/main/java/com/betharley/myapplication/activity/RegisterActivity.kt
@@ -5,9 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.betharley.myapplication.R
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.activity_register.view.*
 
 class RegisterActivity : AppCompatActivity() {
     lateinit var register_register: Button
+
+    lateinit var register_name: TextInputEditText
+    lateinit var register_email: TextInputEditText
+    lateinit var register_password: TextInputEditText
+    lateinit var register_password_repeat: TextInputEditText
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,10 +24,48 @@ class RegisterActivity : AppCompatActivity() {
 
         register_register = findViewById<Button>(R.id.register_register) as Button
 
+        register_name = findViewById(R.id.register_name_edit)
+        register_email = findViewById(R.id.register_email_edit)
+        register_password = findViewById(R.id.register_password_edit)
+        register_password_repeat = findViewById(R.id.register_password_repeat_edit)
+
 
         register_register.setOnClickListener {
             val intent: Intent = Intent(applicationContext, HomeActivity::class.java)
             startActivity( intent )
+        }
+    }
+
+    fun registrarUsuario(){
+        //var usuario: Usuario = Usuario("", "", "")
+
+        var nome = register_name_edit.text.toString()
+        var email = register_email_edit.text.toString()
+        var password = register_password.text.toString()
+        var password_repeat = register_password_repeat_edit.text.toString()
+
+        if( nome.isEmpty() || email.equals("")){
+            register_name_edit.error = "Preencha o nome"
+        }else
+        if( email.isEmpty() || email.equals("")){
+            register_email_edit.error = "Preencha o email"
+        }else
+        if( password.isEmpty() || password.equals("")){
+            register_password.error = "Preencha o password"
+        }else
+        if( password_repeat.isEmpty() || password.equals("")){
+            register_password_repeat_edit.error = "Preencha o password"
+        }
+        else{
+
+            if( password.equals(password_repeat) ){
+                val intent: Intent = Intent(applicationContext, HomeActivity::class.java)
+                startActivity( intent )
+                finish()
+            }else{
+                register_password_repeat_edit.error = "As senhas estão diferentes"
+            }
+
         }
     }
 }
